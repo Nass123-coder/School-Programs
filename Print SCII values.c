a16012523a2622d23cb939bb9451aa5246da14db
Nass123-coder <68243736+Nass123-coder@users.noreply.github.com>
2023-02-19 14:49:06 +0530

    Add files via upload
    Program to print ASCII Values of letters Entered

---
+++ Print SCII values.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char val;
+    int i;
+    printf("Enter a letter\t");
+    scanf("%c", &val);
+    printf("The ascii value of the Letter is: %d\n", val);
+    return 0;
+}
